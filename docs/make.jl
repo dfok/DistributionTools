b0337eca678cbe69a98a3836854112bf8c8fbf05
Dennis Fok <dfok@ese.eur.nl>
2024-06-14 09:10:26 +0200

    Added docs deployment

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,7 @@
 using Documenter, DistributionTools
 push!(LOAD_PATH,"../src/")
 makedocs(modules = [DistributionTools], sitename="DistributionTools.jl")
+
+deploydocs(
+    repo = "github.com/dfok/DistributioTools.jl.git",
+)
